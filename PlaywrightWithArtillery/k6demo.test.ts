fb73d88645ddf7af25c9641205f8d5ad5249e7ef
parthiban murugan <c.m.parthiban1999@gmail.com>
2024-08-26 19:51:09 +0530

    adding PlaywrightWithArtillery

---
+++ PlaywrightWithArtillery/k6demo.test.ts
@@ -0,0 +1,12 @@
+import test from "@playwright/test";
+
+test.describe("", async () => {
+    test.describe.configure({
+        "mode": "serial"
+    })
+
+    test("", () => {
+        var url = "https://test.k6.io/";
+        
+    })
+})
