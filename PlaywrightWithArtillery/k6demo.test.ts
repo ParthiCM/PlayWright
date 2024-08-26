2a1e18bd9a134caff2cf55ca82d90744634e8f2d
parthiban murugan <c.m.parthiban1999@gmail.com>
2024-08-26 20:05:54 +0530

    added k6 demo test

--- PlaywrightWithArtillery/k6demo.test.ts
+++ PlaywrightWithArtillery/k6demo.test.ts
@@ -1,12 +1,29 @@
-import test from "@playwright/test";
+import test, { chromium } from "@playwright/test";
 
-test.describe("", async () => {
+test.describe("Load testing with Playwright and Artillery", async () => {
     test.describe.configure({
         "mode": "serial"
     })
 
-    test("", () => {
+    test("K6 Demo site with Playwright and Artillery", async () => {
         var url = "https://test.k6.io/";
+        var browser = await chromium.launch();
+        var context =  await browser.newContext();
+        const page = await context.newPage();
+        await page.goto(url);
+        await page.click('[href="/flip_coin.php"]');
+        await page.waitForEvent("domcontentloaded");
+
+        for (let index = 0; index < 20; index++) {
+            await page.locator('[value="Bet on heads!"]').click();
+            await page.waitForLoadState("domcontentloaded",{timeout:5000});
+           console.log(`Bet on Heads => ${await page.locator('h2').innerText()}`);
+        }
         
+        for (let index = 0; index < 20; index++) {
+            await page.locator('[value="Bet on tails!"]').click();
+            await page.waitForLoadState("domcontentloaded",{timeout:5000});
+           console.log(`Bet on Heads => ${await page.locator('h2').innerText()}`);
+        }
     })
 })
